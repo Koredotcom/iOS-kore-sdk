191af7c6a5eaf3c2a37eda9e4cf301957e0556ae
Anoop Dhiman <anoop.dhiman@kore.com>
2017-11-15 14:37:15 +0530

    Config changes

--- KoreBotSDK/KoreBotSDKDemo/SDKConfiguration.swift
+++ KoreBotSDK/KoreBotSDKDemo/SDKConfiguration.swift
@@ -16,29 +16,30 @@ class SDKConfiguration: NSObject {
     }
     
     struct botConfig {
-        static let clientId = "cs-ccec5fef-7aa3-5b01-afa5-9c7328b1e7dc" // Copy this value from Bot Builder SDK Settings ex. cs-5250bdc9-6bfe-5ece-92c9-ab54aa2d4285
+        static let clientId = "<client-id>" // Copy this value from Bot Builder SDK Settings ex. cs-5250bdc9-6bfe-5ece-92c9-ab54aa2d4285
         
-        static let clientSecret = "gkn4bUjl6MpZ1jILTSs8i7F5QP/gd5ydXk7l5Ji/eOg=" // Copy this value from Bot Builder SDK Settings ex. Wibn3ULagYyq0J10LCndswYycHGLuIWbwHvTRSfLwhs=
+        static let clientSecret = "<client-secret>" // Copy this value from Bot Builder SDK Settings ex. Wibn3ULagYyq0J10LCndswYycHGLuIWbwHvTRSfLwhs=
         
-        static let botId =  "st-d45e59c9-6753-5cba-8e29-d796b70ee63b" // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. st-acecd91f-b009-5f3f-9c15-7249186d827d
+        static let botId =  "<bot-id>" // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. st-acecd91f-b009-5f3f-9c15-7249186d827d
 
-        static let chatBotName = "Barry Pilot" // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. "Demo Bot"
+        static let chatBotName = "<bot-name>" // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. "Demo Bot"
         
-        static let identity = "demo@kore.com"// This should represent the subject for JWT token. This can be an email or phone number, in case of known user, and in case of anonymous user, this can be a randomly generated unique id.
+        static let identity = "<identity-email> or <random-id>"// This should represent the subject for JWT token. This can be an email or phone number, in case of known user, and in case of anonymous user, this can be a randomly generated unique id.
         
-        static let isAnonymous = true // This should be either true (in case of known-user) or false (in-case of anonymous user).
+        static let isAnonymous = false // This should be either true (in case of known-user) or false (in-case of anonymous user).
     }
     
     struct serverConfig {
-        static let JWT_SERVER = String(format: "http://demo.kore.net:3000/") // Replace it with the actual JWT server URL, if required. Refer to developer documentation for instructions on hosting JWT Server.
+        static let JWT_SERVER = String(format: "http://<jwt-server-host>/") // Replace it with the actual JWT server URL, if required. Refer to developer documentation for instructions on hosting JWT Server.
         
         static func koreJwtUrl() -> String {
             return String(format: "%@users/sts", JWT_SERVER)
         }
         
-        static let BOT_SERVER = String(format: "https://pilot-bots.kore.ai/")
+        static let BOT_SERVER = String(format: "https://bots.kore.ai/")
     }
     
+    // googleapi speech API_KEY
     struct speechConfig {
         static let API_KEY = "AIzaSyCagwsHmUxecD-ZR6OJoL_YAvRBFIXFArQ"
     }
