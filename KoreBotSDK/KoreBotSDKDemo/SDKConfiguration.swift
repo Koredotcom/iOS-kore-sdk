34bfda81f36fa61c7180452e5d2bf54e3ae248e7
Deepak Karadi <deepakprakash.karadi@kore.com>
2018-02-27 12:47:43 +0530

    philips onboarding configuration changes

--- KoreBotSDK/KoreBotSDKDemo/SDKConfiguration.swift
+++ KoreBotSDK/KoreBotSDKDemo/SDKConfiguration.swift
@@ -16,17 +16,17 @@ class SDKConfiguration: NSObject {
     }
     
     struct botConfig {
-        static let clientId = "<client-id>" // Copy this value from Bot Builder SDK Settings ex. cs-5250bdc9-6bfe-5ece-92c9-ab54aa2d4285
+        static let clientId = "Philips onboarding bot" // Copy this value from Bot Builder SDK Settings ex. cs-5250bdc9-6bfe-5ece-92c9-ab54aa2d4285
         
-        static let clientSecret = "<client-secret>" // Copy this value from Bot Builder SDK Settings ex. Wibn3ULagYyq0J10LCndswYycHGLuIWbwHvTRSfLwhs=
+        static let clientSecret = "cs-d6cafc92-7fd9-5600-bdeb-19db57f84dd8" // Copy this value from Bot Builder SDK Settings ex. Wibn3ULagYyq0J10LCndswYycHGLuIWbwHvTRSfLwhs=
         
-        static let botId =  "<bot-id>" // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. st-acecd91f-b009-5f3f-9c15-7249186d827d
+        static let botId =  "st-35169f26-3297-51c3-9656-63c93d9f1e6e" // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. st-acecd91f-b009-5f3f-9c15-7249186d827d
 
-        static let chatBotName = "<bot-name>" // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. "Demo Bot"
+        static let chatBotName = "1TLPeYW6A0idwkUs+qeeCgsXQN8DSBJeK7heH+HzcFg=" // Copy this value from Bot Builder -> Channels -> Web/Mobile Client  ex. "Demo Bot"
         
-        static let identity = "<identity-email> or <random-id>"// This should represent the subject for JWT token. This can be an email or phone number, in case of known user, and in case of anonymous user, this can be a randomly generated unique id.
+        static let identity = "harinder.bommakanti@kore.com"// This should represent the subject for JWT token. This can be an email or phone number, in case of known user, and in case of anonymous user, this can be a randomly generated unique id.
         
-        static let isAnonymous = false // This should be either true (in case of known-user) or false (in-case of anonymous user).
+        static let isAnonymous = true // This should be either true (in case of known-user) or false (in-case of anonymous user).
     }
     
     struct serverConfig {
