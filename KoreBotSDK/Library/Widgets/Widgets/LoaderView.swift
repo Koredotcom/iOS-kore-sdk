d989fec0bbd3dea5ba44841ac91fbd70820b9921
Sowmya Ponangi <sowmya.ponangi@collab.ai>
2018-11-18 13:29:26 +0530

    loader color according to new design

--- KoreBotSDK/Library/Widgets/Widgets/LoaderView.swift
+++ KoreBotSDK/Library/Widgets/Widgets/LoaderView.swift
@@ -112,7 +112,7 @@ class LoaderViewCell: UITableViewCell {
         textLbl.numberOfLines = 2
         
         textLbl.font = UIFont(name: "Helvetica", size: 16.0)
-        textLbl.textColor = UIColor(red: 20/255, green: 50/255, blue: 255/255, alpha: 1)
+        textLbl.textColor = UIColor(red: 189/255, green: 190/255, blue: 199/255, alpha: 1)
         contentView.addSubview(textLbl)
         setupProgressBar()
         
@@ -128,8 +128,8 @@ class LoaderViewCell: UITableViewCell {
         //appearance
         progressBar.progressAppearance = DottedProgressBar.DottedProgressAppearance(
             dotRadius: 8.0,
-            dotsColor: UIColor(red: 60/255, green: 60/255, blue: 65/255, alpha: 0.4),
-            dotsProgressColor: UIColor(red: 43/255, green: 134/255, blue: 179/255, alpha: 1),
+            dotsColor: UIColor(red: 20/255, green: 50/255, blue: 255/255, alpha: 0.4),
+            dotsProgressColor: UIColor(red: 20/255, green: 50/255, blue: 255/255, alpha: 1),
             backColor: UIColor.clear
         )
         progressBar.translatesAutoresizingMaskIntoConstraints = false
