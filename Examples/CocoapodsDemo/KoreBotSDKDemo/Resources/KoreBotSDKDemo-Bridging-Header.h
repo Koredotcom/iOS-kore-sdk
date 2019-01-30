61d1d28d17f0156ab54f5ff9eed4e8d7e2deea32
Srinivas Vasadi <srinivas.vasadi@kore.com>
2019-01-30 19:40:38 +0530

    removed unnecessary pods.

--- Examples/CocoapodsDemo/KoreBotSDKDemo/Resources/KoreBotSDKDemo-Bridging-Header.h
+++ Examples/CocoapodsDemo/KoreBotSDKDemo/Resources/KoreBotSDKDemo-Bridging-Header.h
@@ -11,7 +11,6 @@
 
 #import <AFNetworking/AFNetworking.h>
 #import <AFNetworking/UIImageView+AFNetworking.h>
-#import <TOWebViewController/TOWebViewController.h>
 
 #import "KREAttributedLabel.h"
 #import "KRELayoutManager.h"
