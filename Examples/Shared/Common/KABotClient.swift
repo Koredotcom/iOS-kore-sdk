ef79115c77f53c5f9ef84ae0e64d1d2b579b124d
Kartheek <Kartheek.pagidimarri@kore.com>
2020-05-21 19:20:22 +0530

    Fixed app crash when app come from background to foreground

--- Examples/Shared/Common/KABotClient.swift
+++ Examples/Shared/Common/KABotClient.swift
@@ -367,7 +367,7 @@ open class KABotClient: NSObject {
             context.perform {
                 let resources: Dictionary<String, AnyObject> = ["threadId": botId as AnyObject, "subject": chatBotName as AnyObject, "messages":[] as AnyObject]
                 
-                dataStoreManager.deleteThreadIfRequired(with: botId, completionBlock: { (success) in
+                //dataStoreManager.deleteThreadIfRequired(with: botId, completionBlock: { (success) in
                     dataStoreManager.insertOrUpdateThread(dictionary: resources, with: {(thread1) in
                         self?.thread = thread1
                         try? context.save()
@@ -383,7 +383,7 @@ open class KABotClient: NSObject {
                                 failure?(error!)
                         })
                     })
-                })
+                //})
             }
             }, failure: { (error) in
                 print(error)
--- Examples/Shared/Controllers/AppLaunch/AppLaunchViewController.swift
+++ Examples/Shared/Controllers/AppLaunch/AppLaunchViewController.swift
@@ -72,6 +72,11 @@ class AppLaunchViewController: UIViewController {
             identity = SDKConfiguration.botConfig.identity
         }
         
+        let dataStoreManager: DataStoreManager = DataStoreManager.sharedManager
+        dataStoreManager.deleteThreadIfRequired(with: botId, completionBlock: { (success) in
+            print("Delete Sucess")
+        })
+        
         if !clientId.hasPrefix("<") && !clientSecret.hasPrefix("<") && !chatBotName.hasPrefix("<") && !botId.hasPrefix("<") && !identity.hasPrefix("<") {
             let activityIndicatorView: UIActivityIndicatorView = UIActivityIndicatorView(style: .white)
             activityIndicatorView.center = chatButton.center
